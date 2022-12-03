0a154d9600af712f5de7c57cf1be788a372a115a
olga5bespopovtseva <olinkaa@hotmail.es>
2022-12-03 15:30:51 +0100

    final corrections

--- brain_games/games/prime.py
+++ brain_games/games/prime.py
@@ -4,13 +4,13 @@ from random import randint
 DESCRIPTION = 'Answer "yes" if given number is prime. Otherwise answer "no".'
 
 
-def is_prime(k):
-    if k == 2 or k == 3:
+def is_prime(num):
+    if num == 2 or num == 3:
         return True
-    if k % 2 == 0 or k < 2:
+    if num % 2 == 0 or num < 2:
         return False
-    for i in range(3, int(k ** 0.5) + 1, 2):
-        if k % i == 0:
+    for i in range(3, int(num ** 0.5) + 1, 2):
+        if num % i == 0:
             return False
     return True
 
